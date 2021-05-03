65d2f059586dc68af7732aa9e8b5dbd9d2c2ed78
Lucas Wung <tkoppopwung130@gmail.com>
2021-05-03 14:08:57 -0700

    update on ObjectDetector in index.jsx

--- src/components/objectDetector/index.jsx
+++ src/components/objectDetector/index.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 
 import "@tensorflow/tfjs-backend-cpu";
-import "@tensorflow/tfjs-backend-webgl";
+//import "@tensorflow/tfjs-backend-webgl";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
-
 
 const ObjectDetectorContainer = styled.div`
     display:flex;
@@ -53,11 +52,37 @@ const SelectButton = styled.button`
 
 export function ObjectDetector(props) {
 
+    const fileInputRef = useRef();
+    const [imgData,setImgData] = useState(null);
+
+    const openFilePicker = () => {
+        if(fileInputRef.current) fileInputRef.current.click();
+    }
+
+    const readImage = (file) => {
+        return new Promise((rs, rj) => {
+            const fileReader = new FileReader();
+            fileReader.onload = () => rs(fileReader.result);
+            fileReader.onerror = () => rj(fileReader.error);
+            fileReader.readAsDataURL(file);
+
+        })
+    }
+
+    const onSelectImage= async(e) => {
+        const file = e.target.files[0];
+        const imgData = await readImage(file);
+        setImgData(imgData);
+    }
+
+
+
 
     return <ObjectDetectorContainer>
-        <ObjectDetector>
-            Img
-        </ObjectDetector>
+        <DetectorContainer>
+            {imgData && <img src ={imgData} />}
+        </DetectorContainer>
+        <HiddenFileInput type="file" ref ={fileInputRef} onChange={onSelectImage}> </HiddenFileInput>
         <SelectButton> Select Image</SelectButton>
     </ObjectDetectorContainer>
 }
