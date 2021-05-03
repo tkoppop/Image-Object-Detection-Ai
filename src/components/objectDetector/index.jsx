426e759eb5a1b492447fa8d488b337029d4e24d8
Lucas Wung <tkoppopwung130@gmail.com>
2021-05-03 13:26:51 -0700

    Rename, and finish basic index setup

--- src/components/objectDetector/index.jsx
+++ src/components/objectDetector/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+
 
 const ObjectDetectorContainer = styled.div`
     display:flex;
@@ -31,15 +34,30 @@ const HiddenFileInput = styled.input`
 const SelectButton = styled.button`
     padding: 7px 10px;
     border: 2px solid transparant;
-    background-color: #ffff;
-    border=radius:5px;
-    display:flex;
-    align-items:center;
-    justify-content: center;
-    position: relative;
+    background-color: #fff;
+    color: #0a0f22;
+    font-size: 16px;
+    font-weight:500;
+    outline: none;
+    margin-top: 2em;
+    cursor: pointer;
+    transition: all 260ms ease-in-out;
+
+    &:hover{
+        background-color:transparent;
+        border:2px solid #fff;
+        color: #fff;
+    }
 `;
 
 
 export function ObjectDetector(props) {
 
+
+    return <ObjectDetectorContainer>
+        <ObjectDetector>
+            Img
+        </ObjectDetector>
+        <SelectButton> Select Image</SelectButton>
+    </ObjectDetectorContainer>
 }
